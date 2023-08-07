6dbf61171321e6d2603cc9c5682cb21caf64955b
Wayne <waynefenwick@gmail.com>
2023-08-07 14:06:07 -0500

    Further updates to Portfolio section

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import profilePic from '../assets/Profile-Pic.png';
+import passwordGen from '../assets/password-generator.png';
 
 
 const Content = () => {
   return (
-    <section>
+    <><section>
       <div id="profile-container">
         <div>
           <div id="profile">
-          <img src={profilePic} className="profile-picture" alt="Profile Picture" />
+            <img src={profilePic} className="profile-picture" alt="Profile Picture" />
           </div>
         </div>
-        <div  class="profile">
+        <div className="profile">
           <div id="profile-header">
             <h1><u>Personal Profile</u></h1>
           </div>
@@ -33,7 +34,21 @@ const Content = () => {
         </div>
       </div>
     </section>
-
+    <section id="portfolio-row" className="portfolio">
+      <div className="portfolio-row" id="password-generator">
+        <img src={passwordGen} className="password-generator" alt="Password Generator" />
+        <div className="portfolio-right-column">
+          <h1>Password Generator</h1>
+          <ul className="portfolio-list">
+            <li>Building on HTML & CSS with introduction to JavaScript</li>
+            <li>Purpose was to build a password generator app</li>
+            <li>I added the copy to clipboard option</li>
+            <li>I also changed CSS design of starter code provided</li>
+          </ul>
+          <h4><a id="weblink" href="https://waynefenwick.github.io/Password-Generator/" target="_blank">Visit the Web</a></h4>
+        </div>
+      </div>
+    </section></>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,29 @@
 import React from 'react';
 
 const Header = () => {
+  const handleScrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <header id="top-header">
+    <header>
       <div className="flex-container">
         <h1>WAYNE FENWICK</h1>
       </div>
       <ul className="nav-container">
         <li><a href="#">About Me</a></li>
-        <li><a href="portfolio">Portfolio</a></li>
+        <li><a className="portfolio-link" onClick={() => handleScrollToSection('portfolio-row')}>Portfolio</a></li>
         <li><a href="https://waynefenwick.github.io/Resume/" target="_blank" rel="noreferrer">Contact</a></li>
         <li><a href="https://waynefenwick.github.io/Resume/" target="_blank" rel="noreferrer">Resume</a></li>
       </ul>
+
     </header>
   );
 };
 
 export default Header;
+
 
