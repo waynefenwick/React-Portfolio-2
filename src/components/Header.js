c2f6338185ffb279f992781f63a51ec4b0882c78
Wayne <waynefenwick@gmail.com>
2023-08-09 14:48:28 -0500

    Able to get selected menu items to remain as the selected color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,58 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Header = () => {
+  const [activeLink, setActiveLink] = useState(null);
+
+  const handleLinkClick = (index) => {
+    setActiveLink(index);
+  };
+
   return (
     <header>
       <div>
         <h1 id="top-header">___ WAYNE FENWICK ___</h1>
       </div>
       <div id="nav-container">
-  <ul id="nav-list">
-    <li><a class="link" href="#profile-container">About Me</a></li>
-    <li><a class="link" href="#container">Portfolio</a></li>
-    <li><a class="link" href="#contact">Contact</a></li>
-    <li><a class="link" href="#proficiencies">Resume</a></li>
-  </ul>
-</div>
-
-  </header>
+        <ul id="nav-list">
+          <li>
+            <a
+              className={`link ${activeLink === 0 ? 'active' : ''}`}
+              href="#profile-container"
+              onClick={() => handleLinkClick(0)}
+            >
+              About Me
+            </a>
+          </li>
+          <li>
+            <a
+              className={`link ${activeLink === 1 ? 'active' : ''}`}
+              href="#container"
+              onClick={() => handleLinkClick(1)}
+            >
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a
+              className={`link ${activeLink === 2 ? 'active' : ''}`}
+              href="#contact"
+              onClick={() => handleLinkClick(2)}
+            >
+              Contact
+            </a>
+          </li>
+          <li>
+            <a
+              className={`link ${activeLink === 3 ? 'active' : ''}`}
+              href="#proficiencies"
+              onClick={() => handleLinkClick(3)}
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
   );
 };
 
