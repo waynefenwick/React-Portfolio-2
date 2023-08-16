0d1e8b0a437fa530f07ce0f32696f24290bdc031
Wayne <waynefenwick@gmail.com>
2023-08-15 21:17:14 -0500

    Styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer id="footer-container">
       <div className="footer-icons">
         {/* GitHub */}
         <a
